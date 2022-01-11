8aef862e14933265465d3f1c1a5230dce9899be2
Potatoman248 <nlucero851@gmail.com>
2022-01-11 16:04:37 -0700

    Added basic variable

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Basics
 
 This project will help me remember the basics!
+
+This will help me rember the basics of java.
--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,24 @@
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = 800;
+canvas.width = 1000;
 canvas.height = 600;
+
+console.log("Hello world")
+
+let suit = "spades";
+let face = "queen";
+let value = 10;
+
+let isface = true;
+
+let aceOFspades = {
+suit: "spades",
+Face: "ace",
+Value:1,
+IsFace: false
+};
+
+console.log (aceOFspades);
+
+    
