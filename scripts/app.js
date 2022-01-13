2972ac0f1593ba4ae0d7883f1f3bf580e030fc79
Potatoman248 <nlucero851@gmail.com>
2022-01-12 19:04:20 -0700

    variables based

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1000;
 canvas.height = 600;
 
+
 console.log("Hello world")
 
 let suit = "spades";
@@ -19,6 +20,28 @@ Value:1,
 IsFace: false
 };
 
-console.log (aceOFspades);
+console.log(aceOFspades);
 
-    
+let agesofTable1 = [13,23,33];
+
+console.log(agesofTable1[2]);
+
+let namesAndAges =["Hunter", 13, "patrick", 14, "Tanner", 14.8];
+
+namesAndAges.forEach((n) => {
+    console.log(n); 
+});
+
+let myMoney = "i want lots ans lots of it$$$";
+
+console.log(myMoney.toUpperCase());
+
+console.log("I loke mo money".length);
+
+namesAndAges.forEach((n) => {
+    console.log(n);
+});
+
+let over14 =  agesofTable1.filter((n) => n > 14);
+
+console.log(over14); 
